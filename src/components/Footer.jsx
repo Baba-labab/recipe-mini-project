69be57f0efe556621dff5a6d0f086e721e8d52a3
Barbara Goldbeck <goldbeck.barbara@gmail.com>
2025-08-22 15:15:30 +0200

    added basic details and css to AboutPage. Added GitHub Link to Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,6 @@
 export default function Footer() {
-  return <div className="footer">© 2025 B&Z Recipes. All rights reserved.</div>;
+  return <div className="footer">
+    <span>© 2025 B&Z Recipes.</span>
+    <a href="https://github.com/Baba-labab/recipe-mini-project" target="_blank">Find our GitHub repository here!</a>
+    </div>;
 }
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,7 +1,28 @@
 
 function AboutPage() {
   return (
-    <div>AboutPage</div>
+    <div className="main-content">
+      <h1>About</h1>
+      <h2>Project Description</h2>
+      <p>Creation of a SPA with React</p>
+      <h2>Team</h2>
+      <div className="about-container">
+        <div className="team-container">
+          <h3>Barbara</h3>
+          <img src="" alt="Profile Picture" />
+          <a href="https://github.com/Baba-labab" target="_blank">GitHub</a>
+          <a href="https://www.linkedin.com/in/barbara-goldbeck-7ba383367" target="_blank">LinkedIn</a>
+        </div>
+        <div className="team-container">
+          <h3>Zixuan</h3>
+          <img src="" alt="Profile Picture" />
+          <a href="" target="_blank">GitHub</a>
+          <a href="" target="blank">LinkedIn</a>
+        </div>
+      </div>
+
+    </div>
+
   )
 }
 
